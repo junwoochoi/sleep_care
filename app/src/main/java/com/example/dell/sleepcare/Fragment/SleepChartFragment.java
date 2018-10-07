5a8cedde900aefba7b7f9f52713b2295497456ed
Junu93 <cjw4690@gmail.com>
2018-10-07 22:09:45 +0900

    <수면확인 프래그먼트 생성> - 뒤로갔다가 다시 들어오면 프래그먼트내의 뷰페이저에 뷰가 안뜸................ㅠㅠ

--- app/src/main/java/com/example/dell/sleepcare/Fragment/SleepChartFragment.java
+++ app/src/main/java/com/example/dell/sleepcare/Fragment/SleepChartFragment.java
@@ -1,19 +1,38 @@
 package com.example.dell.sleepcare.Fragment;
 
 
-import android.os.Bundle;
 import android.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.dell.sleepcare.Activitity.MainActivity;
+import com.example.dell.sleepcare.Adapter.SleepChartViewPagerAdapter;
 import com.example.dell.sleepcare.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SleepChartFragment extends Fragment {
+public class SleepChartFragment extends android.support.v4.app.Fragment implements MainActivity.OnBackPressedListener {
 
+    Unbinder unbinder;
+    EnvDayFragment envDayFragment;
+    EnvFragment envFragment;
+    MenuItem prevMenuItem;
+    @BindView(R.id.sleep_chart_view_pager)
+    ViewPager viewPager;
+    @BindView(R.id.bottom_navigation)
+    BottomNavigationView bottomNavigation;
 
     public SleepChartFragment() {
         // Required empty public constructor
@@ -24,7 +43,92 @@ public class SleepChartFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sleep_chart, container, false);
+        View view = inflater.inflate(R.layout.fragment_sleep_chart, container, false);
+        unbinder = ButterKnife.bind(this, view);
+
+        setupViewPager(viewPager);
+
+        bottomNavigation.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.action_env_btn:
+                                viewPager.setCurrentItem(0);
+                                break;
+                            case R.id.action_sleep_btn:
+                                viewPager.setCurrentItem(1);
+                                break;
+                        }
+                        return false;
+                    }
+                });
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (prevMenuItem != null) {
+                    prevMenuItem.setChecked(false);
+                }
+                else
+                {
+                    bottomNavigation.getMenu().getItem(0).setChecked(false);
+                }
+                Log.d("page", "onPageSelected: "+position);
+                bottomNavigation.getMenu().getItem(position).setChecked(true);
+                prevMenuItem = bottomNavigation.getMenu().getItem(position);
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        return view;
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        SleepChartViewPagerAdapter adapter = new SleepChartViewPagerAdapter(getFragmentManager());
+        envDayFragment = new EnvDayFragment();
+        envFragment = new EnvFragment();
+        adapter.addFragment(envDayFragment);
+        adapter.addFragment(envFragment);
+        viewPager.setAdapter(adapter);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+
+//        unbinder.unbind();
+    }
+
+    @Override
+    public void onBack() {
+        Log.e("Other", "onBack()");
+        // 리스너를 설정하기 위해 Activity 를 받아옵니다.
+        MainActivity activity = (MainActivity) getActivity();
+        // 한번 뒤로가기 버튼을 눌렀다면 Listener 를 null 로 해제해줍니다.
+        activity.setOnBackPressedListener(null);
+
+        ((MainActivity) getActivity()).mainContentLayout.setVisibility(View.VISIBLE);
+        ((MainActivity) getActivity()).mainFragmentContainer.setVisibility(View.GONE);
+
+        // Activity 에서도 뭔가 처리하고 싶은 내용이 있다면 하단 문장처럼 호출해주면 됩니다.
+        // activity.onBackPressed();
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        ((MainActivity) context).setOnBackPressedListener(this);
     }
 
 }
