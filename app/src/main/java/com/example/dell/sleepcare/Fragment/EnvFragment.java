df8e03695818fef8a0210f431c2a225116c03184
Junu93 <cjw4690@gmail.com>
2018-10-10 17:26:24 +0900

    <수면확인 프래그먼트 생성> - 날짜를 입력시 처음으로 로딩되는 차트는 잘 불러와지나 그 이후 다른 날짜를 로딩하려하면 하얀화면만 나오는 버그가 존재.

--- app/src/main/java/com/example/dell/sleepcare/Fragment/EnvFragment.java
+++ app/src/main/java/com/example/dell/sleepcare/Fragment/EnvFragment.java
@@ -1,6 +1,8 @@
 package com.example.dell.sleepcare.Fragment;
 
 
+import android.app.ProgressDialog;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
@@ -10,17 +12,36 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.example.dell.sleepcare.Adapter.EnvMarkerView;
 import com.example.dell.sleepcare.Model.EnvResult;
+import com.example.dell.sleepcare.Model.UserEnv;
 import com.example.dell.sleepcare.R;
 import com.example.dell.sleepcare.RESTAPI.EnvService;
 import com.example.dell.sleepcare.RESTAPI.RetrofitClient;
 import com.example.dell.sleepcare.Utils.Constants;
 import com.example.dell.sleepcare.Utils.SharedPrefUtils;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.formatter.IFillFormatter;
+import com.github.mikephil.charting.interfaces.dataprovider.LineDataProvider;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.google.gson.Gson;
 import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 import retrofit2.Call;
@@ -33,6 +54,17 @@ public class EnvFragment extends Fragment implements DatePickerDialog.OnDateSetL
 
     Unbinder unbinder;
     FloatingActionButton calendarFbBtn;
+    String email;
+    Retrofit retrofit;
+    EnvService envService;
+    @BindView(R.id.env_humid_chart)
+    LineChart envHumidChart;
+    @BindView(R.id.env_temp_chart)
+    LineChart envTempChart;
+    @BindView(R.id.env_loud_chart)
+    LineChart envLoudChart;
+    @BindView(R.id.env_light_chart)
+    LineChart envLightChart;
 
 
     public EnvFragment() {
@@ -46,12 +78,45 @@ public class EnvFragment extends Fragment implements DatePickerDialog.OnDateSetL
         View view = inflater.inflate(R.layout.fragment_env, container, false);
         calendarFbBtn = (FloatingActionButton) view.findViewById(R.id.calendar_fb_btn);
         calendarFbBtn.setOnClickListener(this);
-        unbinder = ButterKnife.bind(view);
+        unbinder = ButterKnife.bind(this, view);
+        email = SharedPrefUtils.getInstance(getContext()).getStringExtra("email");
+        retrofit = RetrofitClient.getClient(Constants.API_URL);
+        envService = retrofit.create(EnvService.class);
 
-        String email = SharedPrefUtils.getInstance(getContext()).getStringExtra("email");
-        Retrofit retrofit = RetrofitClient.getClient(Constants.API_URL);
-        EnvService envService = retrofit.create(EnvService.class);
-        Call<EnvResult> resultCall = envService.getEnvList(email);
+        Calendar yesterday = Calendar.getInstance();
+        yesterday.add(Calendar.DATE, -1);
+
+        renderDefaultChart(email, envService, yesterday, 1);
+
+        return view;
+    }
+
+    private void renderDefaultChart(String email, EnvService envService, Calendar yesterday, int mode) {
+
+        //로딩바
+        final ProgressDialog progressDialog;
+        progressDialog = new ProgressDialog(getContext());
+        progressDialog.setMessage("데이터를 받아오는 중...");
+        progressDialog.setTitle("로딩");
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        // show it
+        progressDialog.show();
+        int year = yesterday.get(Calendar.YEAR);
+        int month = yesterday.get(Calendar.MONTH) + 1;
+        int dateOfMonth = yesterday.get(Calendar.DATE);
+        String monthStr = String.valueOf(month);
+        String dateOfMonthStr = String.valueOf(dateOfMonth);
+        if (month < 10) {
+            monthStr = "0" + monthStr;
+        }
+        if (dateOfMonth < 10) {
+            dateOfMonthStr = "0" + dateOfMonthStr;
+        }
+
+        String yesterdayStr = yesterday.get(Calendar.YEAR) + "-"
+                + monthStr + "-"
+                + dateOfMonthStr;
+        Call<EnvResult> resultCall = envService.getEnvList(email, yesterdayStr);
         resultCall.enqueue(new Callback<EnvResult>() {
             @Override
             public void onResponse(Call<EnvResult> call, Response<EnvResult> response) {
@@ -61,6 +126,43 @@ public class EnvFragment extends Fragment implements DatePickerDialog.OnDateSetL
                     Log.i(getClass().toString(), new Gson().toJson(response.body()));
 
 
+                    if (response.body().getUserEnv().size() > 0) {
+                        List<UserEnv> userEnvList = response.body().getUserEnv();
+                        List<Entry> tempEntries = new ArrayList<>();
+                        List<Entry> humidEntries = new ArrayList<>();
+                        List<Entry> loudEntries = new ArrayList<>();
+                        List<Entry> lightEntries = new ArrayList<>();
+                        float i = 1;
+                        for (UserEnv userEnv : userEnvList) {
+                            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSX");
+                            Date date = null;
+                            try {
+                                date = sdf.parse(userEnv.getEnvTime());
+                            } catch (ParseException e) {
+                                e.printStackTrace();
+                            }
+
+                            float timeFloat = (float) date.getTime() / 100 + date.getTime() % 100;
+                            humidEntries.add(new Entry(timeFloat, Float.parseFloat(userEnv.getEnvHumid())));
+                            tempEntries.add(new Entry(timeFloat, Float.parseFloat(userEnv.getEnvTemp())));
+                            loudEntries.add(new Entry(timeFloat, Float.parseFloat(userEnv.getEnvLoud())));
+                            lightEntries.add(new Entry(timeFloat, Float.parseFloat(userEnv.getEnvLight())));
+                            i++;
+                        }
+
+                            setLineChart(humidEntries, envHumidChart, 1);
+                            setLineChart(tempEntries, envTempChart, 2);
+                            setLineChart(loudEntries, envLoudChart, 3);
+                            setLineChart(lightEntries, envLightChart, 4);
+
+                        progressDialog.dismiss();
+
+                    } else {
+                        Log.e(getClass().toString(), new Gson().toJson(response.body()));
+                        Toast.makeText(getContext(), " 수면데이터가 존재하지 않습니다. 우측하단의 아이콘을 클릭하여 다른 날짜를 선택해주세요.", Toast.LENGTH_LONG).show();
+                        progressDialog.dismiss();
+                    }
+
                 } else {
                     Log.e(getClass().toString(), new Gson().toJson(response.errorBody()));
                 }
@@ -69,13 +171,100 @@ public class EnvFragment extends Fragment implements DatePickerDialog.OnDateSetL
 
             @Override
             public void onFailure(Call<EnvResult> call, Throwable t) {
-                Toast.makeText(getContext(), "error", Toast.LENGTH_LONG).show();
+                Toast.makeText(getContext(), "에러가 발생했습니다.", Toast.LENGTH_LONG).show();
                 Log.e(getClass().toString(), t.getMessage());
                 t.printStackTrace();
             }
         });
-        return view;
     }
+
+    private void setLineChart(List<Entry> entries, LineChart envChart, int mode) {
+        envChart.setBackgroundColor(Color.WHITE);
+        envChart.getDescription().setEnabled(false);
+        envChart.setDrawGridBackground(false);
+        // enable scaling and dragging
+        envChart.setDragEnabled(true);
+        envChart.setScaleEnabled(true);
+        envChart.setDoubleTapToZoomEnabled(false);
+        envChart.setVisibleXRangeMaximum(30);
+        // if disabled, scaling can be done on x- and y-axis separately
+        envChart.setPinchZoom(false);
+        envChart.setTouchEnabled(true);
+        EnvMarkerView marker = new EnvMarkerView(getContext(), R.layout.markerview);
+        marker.setChartView(envChart);
+        envChart.setMarker(marker);
+        XAxis x = envChart.getXAxis();
+        x.setTextColor(Color.BLACK);
+        x.setDrawGridLines(false);
+        x.setValueFormatter(new IAxisValueFormatter() {
+            @Override
+            public String getFormattedValue(float value, AxisBase axis) {
+                Date date = new Date();
+                date.setTime((long) value * 100);
+                Calendar cal = Calendar.getInstance();
+                cal.setTime(date);
+                return cal.get(Calendar.HOUR) + "시 " + cal.get(Calendar.MINUTE) + "분";
+            }
+        });
+
+        YAxis y = envChart.getAxisLeft();
+
+        y.setLabelCount(6, false);
+        y.setTextColor(Color.BLACK);
+        y.setPosition(YAxis.YAxisLabelPosition.OUTSIDE_CHART);
+        y.setDrawGridLines(false);
+        y.setAxisLineColor(Color.BLACK);
+
+
+        envChart.getAxisRight().setEnabled(false);
+        LineDataSet dataSet = new LineDataSet(entries, "hello");
+        dataSet.setDrawFilled(true);
+        dataSet.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+        dataSet.setCubicIntensity(0.2f);
+        dataSet.setDrawCircles(false);
+        dataSet.setLineWidth(1.8f);
+        dataSet.setCircleRadius(4f);
+        dataSet.setCircleColor(Color.WHITE);
+        switch (mode) {
+            case 1:
+                dataSet.setColor(Color.rgb(104, 241, 175));
+                dataSet.setFillColor(Color.rgb(104, 241, 175));
+                break;
+            case 2:
+                dataSet.setColor(Color.rgb(20, 190, 204));
+                dataSet.setFillColor(Color.rgb(20, 190, 204));
+                break;
+            case 3:
+                dataSet.setColor(Color.rgb(255, 64, 80));
+                dataSet.setFillColor(Color.rgb(255, 64, 80));
+                break;
+            case 4:
+                dataSet.setColor(Color.rgb(204, 20, 128));
+                dataSet.setFillColor(Color.rgb(204, 20, 128));
+                break;
+        }
+        dataSet.setFillAlpha(255);
+        dataSet.setDrawVerticalHighlightIndicator(false);
+        dataSet.setDrawHorizontalHighlightIndicator(false);
+        dataSet.setFillFormatter(new IFillFormatter() {
+            @Override
+            public float getFillLinePosition(ILineDataSet dataSet, LineDataProvider dataProvider) {
+                return -10;
+            }
+        });
+        dataSet.notifyDataSetChanged();
+        LineData lineData = new LineData(dataSet);
+        lineData.setDrawValues(false);
+        lineData.notifyDataChanged();
+
+
+        envChart.setData(lineData);
+        envChart.getLegend().setEnabled(false);
+        envChart.setMaxVisibleValueCount(30);
+        envChart.notifyDataSetChanged();
+        envChart.invalidate();
+    }
+
 
     @Override
     public void onDestroyView() {
@@ -84,18 +273,41 @@ public class EnvFragment extends Fragment implements DatePickerDialog.OnDateSetL
     }
 
 
-
-
     @Override
     public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
         Toast.makeText(getContext(), year + "년" + monthOfYear + "월" + dayOfMonth + "일", Toast.LENGTH_SHORT).show();
+        Calendar selectedDate = Calendar.getInstance();
+        selectedDate.set(year, monthOfYear, dayOfMonth);
+
+        clearEnvChart(envHumidChart);
+        clearEnvChart(envTempChart);
+        clearEnvChart(envLightChart);
+        clearEnvChart(envLoudChart);
+        renderDefaultChart(email, envService, selectedDate, 2);
+
     }
 
+    private void clearEnvChart(LineChart envChart) {
+        try {
+            envChart.clearValues();
+        } catch (Exception e) {
+            Log.e(getClass().toString(), e.getMessage());
+        }
+        envChart.clear();
+        envChart.invalidate();
+    }
 
 
     @Override
     public void onClick(View view) {
-        Toast.makeText(getContext(), "클릭", Toast.LENGTH_SHORT).show();
+
+        final ProgressDialog progressDialog;
+        progressDialog = new ProgressDialog(getContext());
+        progressDialog.setMessage("데이터를 받아오는 중...");
+        progressDialog.setTitle("로딩");
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        // show it
+        progressDialog.show();
         Calendar now = Calendar.getInstance();
         DatePickerDialog dpd = DatePickerDialog.newInstance(
                 EnvFragment.this,
@@ -104,7 +316,43 @@ public class EnvFragment extends Fragment implements DatePickerDialog.OnDateSetL
                 now.get(Calendar.DAY_OF_MONTH) // Inital day selection
         );
         dpd.setAccentColor(getResources().getColor(R.color.primaryColor));
+        Call<EnvResult> result = envService.getSelectableDays(email);
+        result.enqueue(new Callback<EnvResult>() {
+            @Override
+            public void onResponse(Call<EnvResult> call, Response<EnvResult> response) {
+                if (response.isSuccessful()) {
+                    List<UserEnv> userEnvList = response.body().getUserEnv();
+                    List<Calendar> calendarList = new ArrayList<>();
+                    for (UserEnv userEnv : userEnvList) {
+                        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSX");
+                        Date date = null;
+                        try {
+                            date = sdf.parse(userEnv.getEnvTime());
+                        } catch (ParseException e) {
+                            e.printStackTrace();
+                        }
+                        Calendar cal = Calendar.getInstance();
+                        cal.setTime(date);
+                        if (!calendarList.contains(cal)) {
+                            calendarList.add(cal);
+                        }
+                    }
+                    Calendar[] selectableDays = new Calendar[calendarList.size()];
+                    Log.i(getClass().toString(), "calendarList >> " + calendarList.toString());
+                    dpd.setSelectableDays(calendarList.toArray(selectableDays));
+                }
+                progressDialog.dismiss();
+                dpd.show(getActivity().getFragmentManager(), "DatePickerDialog");
+            }
 
-        dpd.show(getActivity().getFragmentManager(), "DatePickerDialog");
+            @Override
+            public void onFailure(Call<EnvResult> call, Throwable t) {
+                Toast.makeText(getContext(), "오류가 발생했습니다.", Toast.LENGTH_SHORT).show();
+                Log.e(getClass().toString(), t.getMessage());
+                t.printStackTrace();
+                progressDialog.dismiss();
+            }
+        });
+
     }
 }
